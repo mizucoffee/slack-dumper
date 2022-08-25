68941f911f09845b29a83ed9a0753f8406e4b150
Ritsuki Goto <develop@mizucoffee.net>
2022-08-26 00:25:50 +0900

    [add] emoji

--- README.md
+++ README.md
@@ -18,7 +18,10 @@ Denoが必要です。
       - mpim:history
       - mpim:read
       - users:read
+      - team:read
+      - emoji:read
    3. ワークスペースにインストールする
+   4. OAuth & PermissionsページでUser OAuth Tokenを入手する
 2. 以下のコマンドを実行する
     ```bash
     $ deno run --allow-env --allow-read --allow-net --allow-write --allow-run --unstable index.js <slack-token>
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "https://deno.land/x/dotenv/load.ts";
 import { WebClient } from 'https://deno.land/x/slack_web_api/mod.js';
 import { delay } from 'https://deno.land/x/delay@v0.2.0/mod.ts';
 import { download } from "https://deno.land/x/download/mod.ts";
+import { nanoid } from "https://deno.land/x/nanoid/mod.ts"
 
 const token = Deno.args[0] || Deno.env.get("SLACK_TOKEN");
 
@@ -18,22 +19,62 @@ if(!token) {
 }
 const web = new WebClient(token);
 
+const channel_ids = []
+const user_ids = []
+const bot_ids = new Set()
+
 try {
   await Deno.remove("output", { recursive: true });
 } catch(e) {}
 await Deno.mkdir("output/channels", { recursive: true });
 await Deno.mkdir("output/files", { recursive: true });
-await Deno.mkdir("output/users/images", { recursive: true });
+await Deno.mkdir("output/users", { recursive: true });
 
-await dumpUsers();
-await dumpConversations();
+// await dumpUsers();
+// await dumpConversations();
+// await dumpBotInfo();
+await dumpEmojis();
+// await dumpInfo();
+
+async function dumpEmojis() {
+  await delay(waitTime);
+  const res = await web.emoji.list()
+  for(const emoji in res.emoji) {
+    await delay(10);
+    if(res.emoji[emoji].startsWith("alias:")) continue;
+    const filename = `emoji_${nanoid()}.${res.emoji[emoji].split(".").pop()}`;
+    await download(res.emoji[emoji], { file: filename, dir: `output/files/` });
+    res.emoji[emoji] = filename
+  }
+  await Deno.writeTextFile("./output/emojis.json", JSON.stringify(res.emoji, null, 2));
+}
+
+async function dumpInfo() {
+  const res = await web.team.info();
+  const team = res.team;
+  team.channels = channel_ids;
+  team.users = user_ids;
+  await download(team.icon.image_230, { file: `${team.id}.png`, dir: `output/files/` });
+  await Deno.writeTextFile("./output/team.json", JSON.stringify(team, null, 2));
+}
+
+async function dumpBotInfo() {
+  for(const b of bot_ids) {
+    await delay(waitTime);
+    const res = await web.bots.info({ bot: b });
+    const bot = res.bot
+    await download(bot.icons.image_72, { file: `${bot.id}.png`, dir: `output/files/` });
+    await Deno.writeTextFile(`./output/users/${bot.id}.json`, JSON.stringify(bot, null, 2));
+  }
+}
 
 async function dumpUsers(cursor = null) {
   await delay(waitTime);
   const users = await web.users.list({ cursor });
   for(const user of users.members) {
     await Deno.writeTextFile(`output/users/${user.id}.json`, JSON.stringify(user, null, 2));
-    await download(user.profile.image_192, { file: `${user.id}.png`, dir: `output/users/images/` });
+    user_ids.push(user.id);
+    await download(user.profile.image_192, { file: `${user.id}.png`, dir: `output/files/` });
   }
 
   if(users.response_metadata.next_cursor) {
@@ -48,6 +89,10 @@ async function dumpConversations(cursor = null) {
   for(const channel of conversations.channels) {
     const messages = await fetchMessages(channel.id);
     channel.messages = messages;
+    for(const message of messages) {
+      if(message.bot_id) bot_ids.add(message.bot_id);
+    }
+    channel_ids.push(channel.id);
     await Deno.writeTextFile(`./output/channels/${channel.id}.json`, JSON.stringify(channel, null, 2));
   }
 
@@ -63,7 +108,7 @@ async function fetchMessages(channelId, cursor = null) {
     m.files = m.files ? await Promise.all(m.files.map(async f => {
       if(!f.url_private_download) return f;
       await download(f.url_private, { file: `${f.id}_${f.name}`, dir: `output/files/` }, { headers: { Authorization: `Bearer ${token}` } });
-      f.filepath = `files/${f.id}_${f.name}`;
+      f.filepath = `${f.id}_${f.name}`;
       return f;
     })) : [];
     return m;
@@ -76,3 +121,9 @@ async function fetchMessages(channelId, cursor = null) {
 
   return messages;
 }
+
+// 関連ページを取得する
+// 絵文字を取得する
+// リプライ
+// コードブロック
+// ファイル埋め込み
